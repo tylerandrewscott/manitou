f1a7a235c2cf5217ed9d7996c4a861d12c46f734
Tyler Scott <master@rstudio-premptible.c.scott-davis-remote.internal>
2021-03-17 19:11:19 +0000

    finalize gof comps

--- policypolitics/code/prep_work/generate_national_forest_data.R
+++ policypolitics/code/prep_work/generate_national_forest_data.R
@@ -8,23 +8,10 @@ cores = detectCores()-2
 first_year = 2000;last_year = 2020
 # projection to use for all spatial data
 albersNA <- "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-110 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m"
+admin_districts <- readRDS('policypolitics/prepped/admin_units_clean.RDS')
+admin_districts <- st_transform(admin_districts,crs = st_crs(albersNA))
 
 # create a temporary folder that stores downloaded shapefile, extract contents and load
-
-
-td = tempdir()
-admin_url = "https://data.fs.usda.gov/geodata/edw/edw_resources/shp/S_USA.AdministrativeForest.zip"
-tf = tempfile(tmpdir=td, fileext=".zip")
-download.file(admin_url, tf)
-fname = unzip(tf, list=TRUE)
-unzip(tf, files=fname$Name, exdir=td, overwrite=TRUE)
-fpath = file.path(td, grep('shp$',fname$Name,value=T))
-admin_districts <- st_read(fpath)
-admin_districts <- st_transform(admin_districts,crs = st_crs(albersNA))
-# fix bad polygons
-bad_polys = !st_is_valid(admin_districts)
-admin_districts[bad_polys,] <- st_make_valid(admin_districts[bad_polys,])
-
 admin_districts$FORESTORGC = as.character(admin_districts$FORESTORGC)
 admin_districts$FOREST_ID = admin_districts$FORESTORGC
 admin_districts$FOREST_ID = formatC(admin_districts$FORESTORGC,width=4,flag = 0)
